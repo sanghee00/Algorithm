4578425420b04a82bafa4194f4abca7e4fe77e90
Sang Hee Han <77792853+sanghee00@users.noreply.github.com>
2025-03-03 16:52:46 +0900

    [Silver II] Title: DFS와 BFS, Time: 268 ms, Memory: 20368 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -4,97 +4,86 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    public static List<Integer>[] edgeList;
+    public static List<Integer>[] adjList;
+    public static List<Integer> result;
     public static boolean[] visited;
-    public static List<Integer> dfsResult = new ArrayList<>();
-    public static List<Integer> bfsResult = new ArrayList<>();
 
     public static void solution(int[][] graph, int startNode, int n) {
-        edgeList = new ArrayList[n + 1];
+        adjList = new ArrayList[n + 1];
         visited = new boolean[n + 1];
+        result = new ArrayList<>();
 
-        for (int i = 0; i < edgeList.length; i++) {
-            edgeList[i] = new ArrayList<>();
+        for (int i = 0; i < adjList.length; i++) {
+            adjList[i] = new ArrayList<>();
         }
 
         for (int[] i : graph) {
-            edgeList[i[0]].add(i[1]);
-            edgeList[i[1]].add(i[0]);
+            adjList[i[0]].add(i[1]);
+            adjList[i[1]].add(i[0]);
         }
 
-        for (List<Integer> integers : edgeList) {
-            integers.sort(Comparator.comparingInt(o1 -> o1));
+        for (int i = 0; i < adjList.length; i++) {
+            Collections.sort(adjList[i]);
         }
 
-//        System.out.println(Arrays.toString(edgeList));
         dfs(startNode);
-        visited = new boolean[n + 1]; // 초기화
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        result = new ArrayList<>();
+        visited = new boolean[n + 1];
         bfs(startNode);
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
     }
 
-    public static void dfs(int startNode) {
-        dfsResult.add(startNode);
-        visited[startNode] = true;
+    public static void dfs(int node) {
+        visited[node] = true;
+        result.add(node);
 
-        for (int i : edgeList[startNode]) {
-            if (!visited[i]) {
-                dfs(i);
+        for (int now : adjList[node]) {
+            if (!visited[now]) {
+                dfs(now);
             }
         }
     }
 
     public static void bfs(int startNode) {
         ArrayDeque<Integer> queue = new ArrayDeque<>();
-        visited[startNode] = true;
         queue.add(startNode);
+        visited[startNode] = true;
 
-        // 비어 있지 않으면
         while (!queue.isEmpty()) {
             int poll = queue.poll();
-            bfsResult.add(poll);
+            result.add(poll);
 
-            for (int i : edgeList[poll]) {
-                if (!visited[i]) {
-                    queue.add(i);
-                    visited[i] = true;
+            for (int now : adjList[poll]) {
+                if (!visited[now]) {
+                    visited[now] = true;
+                    queue.add(now);
                 }
             }
-
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken()); // 정점의 개수
         int m = Integer.parseInt(st.nextToken()); // 간선의 개수
-        int v = Integer.parseInt(st.nextToken()); // 시작 정점
+        int v = Integer.parseInt(st.nextToken()); // 시작할 노드
 
-        int[][] graph = new int[m + 1][2];
-        for (int i = 1; i < m + 1; i++) {
+        int[][] graph = new int[m][2];
+
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             graph[i][0] = Integer.parseInt(st.nextToken());
             graph[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        /*for (int i = 0; i < graph.length; i++) {
-            for (int j = 0; j < graph[i].length; j++) {
-                System.out.print(graph[i][j] + " ");
-            }
-            System.out.println();
-        }
-*/
-
         solution(graph, v, n);
-
-        for (int i : dfsResult) {
-            System.out.print(i + " ");
-        }
-
-        System.out.println();
-        for (int i : bfsResult) {
-            System.out.print(i + " ");
-        }
     }
 }
