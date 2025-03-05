5126392383461fb31e2403c365d6e9d0cd5ad416
Sang Hee Han <77792853+sanghee00@users.noreply.github.com>
2025-03-05 17:49:06 +0900

    [Silver II] Title: 촌수계산, Time: 104 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -7,79 +7,62 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static List<Integer>[] person; // 받을 사람
-    public static int count; // 카운트
-    public static int result = -1; // 카운트
-    public static boolean[] visited; // 방문 처리
-    public static int end;
+    static boolean[] visited;
+    static List<Integer>[] adjList;
+    static int cnt;
+    static int result = -1;
+    static int end;
 
-    public static void solution(int[][] graph, int start, int n) {
-        person = new ArrayList[n + 1];
-
-        for (int i = 0; i < person.length; i++) {
-            person[i] = new ArrayList<>();
-        }
-
-        for (int[] edge : graph) {
-            person[edge[0]].add(edge[1]);
-            person[edge[1]].add(edge[0]);
-        }
-
-//        System.out.println(Arrays.toString(person));
+    public static void solution(int n, int[][] graph, int start) {
+        adjList = new ArrayList[n + 1];
         visited = new boolean[n + 1];
+
+        for (int i = 0; i < adjList.length; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int[] i : graph) {
+            adjList[i[0]].add(i[1]);
+            adjList[i[1]].add(i[0]);
+        }
 
         dfs(start);
     }
 
     public static void dfs(int start) {
-        visited[start] = true; // 방문 후 true
+        visited[start] = true;
         if (start == end) {
-            result = count;
+            result = cnt;
             return;
         }
 
-        for (int n : person[start]) {
-            // 방문하지 않았다면
-            if(!visited[n]) {
-                count++;
+        for (int n : adjList[start]) {
+            if (!visited[n]) {
+                cnt++;
                 dfs(n);
-                count--;
+                cnt--;
             }
         }
-
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 사람 수
+        int n = Integer.parseInt(br.readLine()); // 전체 사람 수
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int person1 = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
 
-        int p2 = Integer.parseInt(st.nextToken()); // 사람1
-        int p1 = Integer.parseInt(st.nextToken()); // 사람2
-        end = p2;
+        int m = Integer.parseInt(br.readLine()); // 관계의 수
 
-        int m = Integer.parseInt(br.readLine()); // 간선
-        int[][] graph = new int[m + 1][2];
+        int[][] graph = new int[m][2];
 
-        for (int i = 1; i < m + 1; i++) {
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 2; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
-            }
+            graph[i][0] = Integer.parseInt(st.nextToken());
+            graph[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        /*for (int i = 0; i < graph.length; i++) {
-            for (int j = 0; j < 2; j++) {
-                System.out.print(graph[i][j] + " ");
-            }
-            System.out.println();
-
-        }*/
-
-
-        solution(graph, p1, n);
-
+        solution(n, graph, person1);
         System.out.println(result);
     }
 }
-
