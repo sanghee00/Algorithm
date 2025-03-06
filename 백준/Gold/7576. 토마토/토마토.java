7cfe0104ee5dba87434e0f162e1f3ab446e07ef3
Sang Hee Han <77792853+sanghee00@users.noreply.github.com>
2025-03-06 18:53:29 +0900

    [Gold V] Title: 토마토, Time: 580 ms, Memory: 103772 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,97 +1,92 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static Tomato[][] box;
-    static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+    static Queue<Tomato> queue = new ArrayDeque<>();
+    static int m;
+    static int n;
+    static boolean[][] visited;
+    static int[][] box;
 
-    public static class Tomato {
+    static class Tomato {
         int x;
         int y;
-        int day;
+        int value;
 
-        public Tomato(int x, int y, int day) {
+        public Tomato(int x, int y, int value) {
             this.x = x;
             this.y = y;
-            this.day = day;
+            this.value = value;
         }
     }
 
-    public static void bfs(int M, int N, ArrayDeque<Tomato> queue) {
+    public static void bfs() {
 
         while (!queue.isEmpty()) {
-            Tomato poll = queue.poll();
+            Tomato tomato = queue.poll();
 
             for (int i = 0; i < 4; i++) {
-                int xx = dx[i] + poll.x;
-                int yy = dy[i] + poll.y;
+                int nx = dx[i] + tomato.x;
+                int ny = dy[i] + tomato.y;
 
-                if (xx >= 0 && yy >= 0 && xx < M && yy < N && !visited[yy][xx] && box[yy][xx].day != -1) {
-                    visited[yy][xx] = true;
-                    box[yy][xx].day = poll.day + 1;
-                    queue.add(box[yy][xx]);
+                if (nx >= 0 && ny >= 0 && nx < m && ny < n && !visited[ny][nx] && box[ny][nx] == 0) {
+                    visited[ny][nx] = true;
+                    box[ny][nx] = tomato.value + 1;
+                    queue.add(new Tomato(nx, ny, tomato.value + 1));
                 }
             }
         }
-
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        ArrayDeque<Tomato> queue = new ArrayDeque<>();
 
-        int M = Integer.parseInt(st.nextToken()); // 가로
-        int N = Integer.parseInt(st.nextToken()); // 세로
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
 
-        box = new Tomato[N][M];
-        visited = new boolean[N][M];
+        box = new int[n][m];
+        visited = new boolean[n][m];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                int tomato = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < m; j++) {
+                int num = Integer.parseInt(st.nextToken());
 
-                // 토마토가 있는 곳은 바로 큐에 넣는다.
-                if (tomato == 1) {
+                if (num == 1) {
+                    queue.add(new Tomato(j, i, 1));
                     visited[i][j] = true;
-                    queue.add(new Tomato(j, i, tomato));
                 }
-                box[i][j] = new Tomato(j, i, tomato);
+
+                box[i][j] = num;
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (box[i][j].day != 0) {
-                    bfs(M, N, queue);
+        bfs();
+
+        boolean result = false;
+        int total = Integer.MIN_VALUE;
+        for (int[] i : box) {
+            for (int j : i) {
+                if (j == 0) {
+                    result = true;
                 }
+                total = Math.max(total, j);
             }
         }
 
 
-        System.out.println(check(N, M));
+        if (result) {
+            System.out.println(-1);
+        } else {
+            System.out.println(total - 1);
+        }
     }
-    public static int check(int N, int M) {
-        int max = box[0][0].day;
-        int day;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                day = box[i][j].day;
-                if (day == 0) {
-                    return -1;
-                }
-
-                if (max < day) {
-                    max = day;
-                }
-            }
-        }
-
-        return max - 1;
-    }
-
-
 }
