38a371ce119758af0dd3bd7ad4bb9f543afcbafb
Sang Hee Han <77792853+sanghee00@users.noreply.github.com>
2025-03-07 21:39:24 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14640 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static Node[][] map;
+    static boolean[][] visited;
+    static Node endPoint;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int n;
+    static int m;
+
+    public static class Node {
+        int x;
+        int y;
+        int cnt;
+
+        public Node(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(cnt);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new Node[n][m];
+        visited = new boolean[n][m];
+        endPoint = new Node(m, n, 1);
+
+        for (int i = 0; i < n; i++) {
+            String s =  br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = new Node(j, i, s.charAt(j) - '0');
+            }
+        }
+
+        bfs(0, 0);
+
+        System.out.println(map[endPoint.y - 1][endPoint.x - 1].cnt);
+    }
+
+    public static void bfs(int x, int y) {
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(x, y, 1));
+        visited[y][x] = true;
+
+        while (!queue.isEmpty()) {
+            Node pollNode = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = dx[i] + pollNode.x;
+                int ny = dy[i] + pollNode.y;
+
+                if (nx >= 0 && ny >= 0 && nx < m && ny < n && !visited[ny][nx] && map[ny][nx].cnt == 1) {
+                    visited[ny][nx] = true;
+                    map[ny][nx].cnt = pollNode.cnt + 1;
+                    queue.add(new Node(nx, ny, pollNode.cnt + 1));
+                }
+            }
+        }
+    }
+}
