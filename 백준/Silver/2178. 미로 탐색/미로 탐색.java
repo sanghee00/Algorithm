0155e1570cfbc9e3af04f2e21ff9f39f5e38a5d0
Sang Hee Han <77792853+sanghee00@users.noreply.github.com>
2025-03-11 18:50:14 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,14 +1,18 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static Node[][] map;
+    static int[][] map;
     static boolean[][] visited;
-    static Node endPoint;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int n;
     static int m;
+    static int n;
 
     public static class Node {
         int x;
@@ -20,39 +24,33 @@ public class Main {
             this.y = y;
             this.cnt = cnt;
         }
-
-        @Override
-        public String toString() {
-            return String.valueOf(cnt);
-        }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
 
-        map = new Node[n][m];
+        n = Integer.parseInt(st.nextToken()); // 세로
+        m = Integer.parseInt(st.nextToken()); // 가로
+        map = new int[n][m];
         visited = new boolean[n][m];
-        endPoint = new Node(m, n, 1);
 
         for (int i = 0; i < n; i++) {
-            String s =  br.readLine();
+            String s = br.readLine();
             for (int j = 0; j < m; j++) {
-                map[i][j] = new Node(j, i, s.charAt(j) - '0');
+                map[i][j] = s.charAt(j) - '0';
             }
         }
 
-        bfs(0, 0);
+        bfs();
 
-        System.out.println(map[endPoint.y - 1][endPoint.x - 1].cnt);
+        System.out.println(map[n - 1][m - 1]);
+
     }
 
-    public static void bfs(int x, int y) {
+    public static void bfs() {
         Queue<Node> queue = new ArrayDeque<>();
-        queue.add(new Node(x, y, 1));
-        visited[y][x] = true;
+        queue.add(new Node(0, 0, 1));
 
         while (!queue.isEmpty()) {
             Node pollNode = queue.poll();
@@ -61,12 +59,13 @@ public class Main {
                 int nx = dx[i] + pollNode.x;
                 int ny = dy[i] + pollNode.y;
 
-                if (nx >= 0 && ny >= 0 && nx < m && ny < n && !visited[ny][nx] && map[ny][nx].cnt == 1) {
+                if (nx >= 0 && ny >= 0 && nx < m && ny < n && !visited[ny][nx] && map[ny][nx] == 1) {
                     visited[ny][nx] = true;
-                    map[ny][nx].cnt = pollNode.cnt + 1;
+                    map[ny][nx] = pollNode.cnt + 1;
                     queue.add(new Node(nx, ny, pollNode.cnt + 1));
                 }
             }
+
         }
     }
 }
