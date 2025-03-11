d1489fb06dc57a71205eabbb0c3d46322c69d481
Sang Hee Han <77792853+sanghee00@users.noreply.github.com>
2025-03-11 18:58:39 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -4,17 +4,19 @@ import java.io.InputStreamReader;
 import java.math.BigInteger;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BigInteger[] dp = new BigInteger[91];
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+    static BigInteger[] dp;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        dp = new BigInteger[91];
         dp[0] = BigInteger.valueOf(0);
         dp[1] = BigInteger.valueOf(1);
-        for (int i = 2; i <= n; i++) {
+
+        for (int i = 2; i < a + 1; i++) {
             dp[i] = dp[i - 1].add(dp[i - 2]);
         }
 
-        System.out.println(dp[n]);
+        System.out.println(dp[a]);
     }
 }
