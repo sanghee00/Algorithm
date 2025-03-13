e7c8e044a0472aba58d5e6fa8cf43f8bf0db0a5c
Sang Hee Han <77792853+sanghee00@users.noreply.github.com>
2025-03-13 09:08:46 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,5 +1,3 @@
-
-
 import java.io.*;
 
 public class Main {
@@ -9,30 +7,20 @@ public class Main {
 
         for (int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
-            int[] dp = new int[n + 1];
+            int[] dp = new int[11];
 
-            if (n < 4) {
-                int[] dp1 = new int[4];
-                dp1[0] = 0;
-                dp1[1] = 1;
-                dp1[2] = 2;
-                dp1[3] = 4;
-                System.out.println(dp1[n]);
-            } else {
-                dp[0] = 0;
-                dp[1] = 1;
-                dp[2] = 2;
-                dp[3] = 4;
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
 
-                for (int j = 4; j <= n; j++) {
-                    dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
-                }
-                System.out.println(dp[n]);
-
+            for (int j = 4; j <= n; j++) {
+                dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
             }
-
+            System.out.println(dp[n]);
 
         }
+
+
     }
 }
-
