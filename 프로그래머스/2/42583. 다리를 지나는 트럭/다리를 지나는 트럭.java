679efd40d95b84b44e278f0586c84af9e8a2bab8
Sang Hee Han <77792853+sanghee00@users.noreply.github.com>
2026-03-18 12:32:51 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 41.90 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -13,20 +13,21 @@ class Solution {
             bridge.add(0);
         }
         
-        
+        int currentBridgeWeight = 0;
         while(!bridge.isEmpty()) {
             answer++; // time 1초 증가
             
-            bridge.poll(); // 하나를 뺀다.
+            currentBridgeWeight -= bridge.poll(); // 하나를 뺀다.
             
+            // 대기 중인 트럭이 있을시 실행
             if (!truckWeightsQueue.isEmpty()) {
                 // 해당 원소로 최대 무게 초과 하는지 확인
-                int sumWeight = bridge.stream().mapToInt(Integer::intValue).sum(); // 최종 무게
-                if ((sumWeight + truckWeightsQueue.peek()) > weight) {
+                if ((currentBridgeWeight + truckWeightsQueue.peek()) > weight) {
                     bridge.add(0); // 무게가 초과하면 0이 들어감
                 } else {
-                    int firstTruck = truckWeightsQueue.poll(); // 실제 큐에서 맨앞에 있는 원소를 뺌
-                    bridge.add(firstTruck);
+                    int nextTruck = truckWeightsQueue.poll(); // 실제 큐에서 맨앞에 있는 원소를 뺌
+                    bridge.add(nextTruck);
+                    currentBridgeWeight += nextTruck;
                 }
             }
             
