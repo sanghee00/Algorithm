cce59bddb65ae1964d93134fcd107dfaa9447ab3
Sang Hee Han <77792853+sanghee00@users.noreply.github.com>
2026-03-19 18:18:08 +0900

    [level 2] Title: 주식가격, Time: 73.08 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,18 +1,26 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        int pricesLastIndex = prices.length - 1;
+        Stack<Integer> stack = new Stack<>();
         
         for (int i = 0; i < prices.length; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
-                answer[i]++;
-                
-                if (prices[i] > prices[j]) {
-                    break;
-                }
+            
+            // stack이 비워져있지 않거나, 전 가격이 더 크면 떨어진 시간을 구한다.
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int popIndex = stack.pop();
+                answer[popIndex] = i - popIndex;
             }
             
+            stack.push(i);
         }
+        
+        while (!stack.isEmpty()) {
+            int popIndex = stack.pop();
+            answer[popIndex] = (prices.length - 1) - popIndex;
+        }
+    
         
         return answer;
     }
