6e9fc9e398c30130dc20d32ce5278ed54b7bb0dd
Sang Hee Han <77792853+sanghee00@users.noreply.github.com>
2026-03-21 17:16:29 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -11,9 +11,10 @@ class Solution {
         // *= 을 해야되서 기본 1로 설정, 0으로 하면 다 0임
         int answer = 1;
         for (int count : map.values()) {
-            answer *= (count + 1);
+            answer *= (count + 1); // + 1을 하는 이유는 안 입는다는 조건이 있기 때문에
         }
         
+        // 곱의 법칙을 하는 경우, { "안 입음", "안 입음" } 이 존재 해버리기 때문에 1을 빼야 한다.
         return answer - 1;
     }
 }
